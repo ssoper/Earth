d0a37b2a3b625999441e315f1f1196d663d0c533
Sean Soper <sean.soper@gmail.com>
2016-04-22 14:42:13 -0400

    globe now spins

--- Earth/EarthScene.swift
+++ Earth/EarthScene.swift
@@ -25,6 +25,13 @@ class EarthScene: SCNScene {
         lightNode.light = light
         lightNode.position = SCNVector3(x: -1.5, y: 1.5, z: 4.5)
         self.rootNode.addChildNode(lightNode)
+
+        let spin = CABasicAnimation(keyPath: "rotation")
+        spin.fromValue = NSValue(SCNVector4: SCNVector4(x: 0, y: 0, z: 0, w: 0))
+        spin.toValue = NSValue(SCNVector4: SCNVector4(x: 0, y: 1, z: 0, w: Float(4*M_PI)))
+        spin.duration = 30
+        spin.repeatCount = .infinity
+        sphereNode.addAnimation(spin, forKey: "spin around")
     }
     
     required init?(coder aDecoder: NSCoder) {
